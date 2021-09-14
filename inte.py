6b6438c3b99604e12fcad78f1d2c1ede6e3145ea
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-13 23:14:26 -0700

    Add files via upload

---
+++ inte.py
@@ -0,0 +1,22 @@
+
+def total (a, b):
+    total = a + b
+    if total in range (100, 1000):
+        return 500
+    else:
+        return total
+
+print(total(350, 630))
+print(total(450, 730))
+print(total(650, 930))
+print(total(231, 312))
+print(total(3323, 9))
+print(total(344, 965))
+print(total(632, 938))
+print(total(948, 10))
+print(total(93, 9123))
+
+
+
+
+
